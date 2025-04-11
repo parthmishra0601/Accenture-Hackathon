157f4dd5f55fc89907422ff01e201dd40d612ffa
Parth Mishra <114911403+parthmishra0601@users.noreply.github.com>
2025-04-11 17:28:34 +0530

    Update ConversationsDetail.js

--- accenture/src/Components/ConversationsDetail.js
+++ accenture/src/Components/ConversationsDetail.js
@@ -7,7 +7,7 @@ const ConversationDetails = () => {
   useEffect(() => {
     const fetchTickets = async () => {
       try {
-        const res = await axios.get("http://127.0.0.1:5000/api/conversations");
+        const res = await axios.get("https://accenture-hackathon-14.onrender.com");
         setTickets(res.data);
       } catch (error) {
         console.error("Error fetching conversations:", error);
